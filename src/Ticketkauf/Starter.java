825835121dfad466f875773be1e9d84564faa543
ÜK M-223 <christo.ch@hotmail.com>
2021-11-15 17:28:10 +0100

    Tag 2

--- src/Ticketkauf/Starter.java
+++ src/Ticketkauf/Starter.java
@@ -1,10 +1,13 @@
 package Ticketkauf;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.ComboBoxListCell;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -18,33 +21,32 @@ public class Starter extends Application {
     private Spinner<Integer> anzahl;
     private final Double preis = 200.00;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage){
+    public void start(Stage primaryStage) {
 
 
         GridPane gridPane = new GridPane();
 
-        gridPane.add(new Label("Name"), 0,0);
+        gridPane.add(new Label("Name"), 0, 0);
 
         TextField name = new TextField();
-        gridPane.add(name,1,0);
+        gridPane.add(name, 1, 0);
 
 //        gridPane.add(new TextField(""), 1,0);
-        gridPane.add(new Label("Vorname"), 0,1);
+        gridPane.add(new Label("Vorname"), 0, 1);
 
         TextField vorname = new TextField();
-        gridPane.add(vorname,1,1);
+        gridPane.add(vorname, 1, 1);
 
-        gridPane.add(new Label("Geburtsdatum"), 0,2);
+        gridPane.add(new Label("Geburtsdatum"), 0, 2);
 
         DatePicker datepicker = new DatePicker();
-        gridPane.add(datepicker,1,2);
-        gridPane.add(new Label("Volljährig"), 0,3);
+        gridPane.add(datepicker, 1, 2);
+        gridPane.add(new Label("Volljährig"), 0, 3);
 
         ToggleGroup group = new ToggleGroup();
         RadioButton button1 = new RadioButton("Ja");
@@ -53,14 +55,25 @@ public class Starter extends Application {
         RadioButton button2 = new RadioButton("Nein");
         button2.setToggleGroup(group);
 
-        HBox hbox = new HBox(4,button1,button2);
-        gridPane.setConstraints(hbox,1,3);
+        HBox hbox = new HBox(4, button1, button2);
+        gridPane.setConstraints(hbox, 1, 3);
         gridPane.getChildren().add(hbox);
 
-        gridPane.add(new Label("Anzahl"), 0,4);
-        anzahl = new Spinner<Integer>(0, 100,0);
+        gridPane.add(new Label("Anzahl"), 0, 4);
+        anzahl = new Spinner<Integer>(0, 100, 0);
         gridPane.setConstraints(anzahl, 1, 4);
         gridPane.getChildren().add(anzahl);
+
+        gridPane.add(new Label("Event"), 0, 5);
+        ListView<String> list = new ListView<String>();
+        list.setPrefSize(200, 250);
+        list.setEditable(true);
+        ObservableList<String> items = FXCollections.observableArrayList(
+                "A = Kino", "B = Degustation", "C = Tanz", "D = Comedy Show");
+        list.setItems(items);
+        list.setCellFactory(ComboBoxListCell.forListView(items));
+        gridPane.setConstraints(list, 1, 5);
+        gridPane.getChildren().add(list);
 
         /*gridPane.add(new Label("Preis"), 0,5);
         preis = new Spinner<Double>(0, 100,0);
@@ -83,8 +96,8 @@ public class Starter extends Application {
                 Integer anz = anzahl.getValue();
                 Double price = anzahl.getValue() * preis;
 
-                
-                Person person = new Person(pID, lastname, firstname,  bday, volljaehrig, anz, price);
+
+                Person person = new Person(pID, lastname, firstname, bday, volljaehrig, anz, price);
 
                 System.out.println(person.getID());
                 System.out.println(person.getName());
@@ -96,13 +109,25 @@ public class Starter extends Application {
 
             }
         });
-        gridPane.add(submit, 1, 6);
+        gridPane.add(submit, 1, 10);
 
         gridPane.setPadding(new Insets(20, 20 , 20 ,20));
         gridPane.setVgap(10);
         gridPane.setHgap(10);
+       /* BorderPane border = new BorderPane();
 
-        Scene scene = new Scene(gridPane, 300, 250);
+
+        AnchorPane anchorPane = new AnchorPane();
+        Button ok = new Button();
+        Button abbrechen = new Button();
+        anchorPane.setLeftAnchor(ok, 10.0);
+        anchorPane.setRightAnchor(abbrechen, 10.0);
+        border.getChildren().add(anchorPane);*/
+
+
+
+
+        Scene scene = new Scene(gridPane, 300, 300);
 
         primaryStage.setTitle("Ticket");
         primaryStage.setScene(scene);
---
+++ src/Ticketkauf_anchor/Ticket.java
@@ -0,0 +1,79 @@
+package Ticketkauf_anchor;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.cell.ComboBoxListCell;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+//falls java awt dann löschen und neu importieren.
+
+
+public class Ticket extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        BorderPane borderPane = new BorderPane();
+        Scene scene = new Scene(borderPane,400,400);
+
+        Label text = new Label("Ticket kaufen");
+        borderPane.setAlignment(text, Pos.CENTER);
+        borderPane.setTop(text);
+
+        GridPane gridPane = new GridPane();
+        borderPane.setCenter(gridPane);
+        gridPane.setPadding(new Insets(20, 20,20,20));
+
+
+        gridPane.add(new Label("Event"),0,0);
+        ListView<String> list = new ListView<String>();
+        list.setPrefSize(250, 100);
+        ObservableList<String> items = FXCollections.observableArrayList(
+                "A = Kino", "B = Degustation", "C = Tanz", "D = Comedy Show");
+        list.setItems(items);
+        list.setCellFactory(ComboBoxListCell.forListView(items));
+        gridPane.setConstraints(list, 1, 0);
+        gridPane.getChildren().add(list);
+
+        gridPane.setHgap(50);
+
+
+        AnchorPane anchorPane = new AnchorPane();
+        Button ok = new Button();
+        Button abbrechen = new Button();
+        anchorPane.setLeftAnchor(ok, 10.0);
+        anchorPane.setRightAnchor(abbrechen, 10.0);
+
+
+        borderPane.setBottom(anchorPane);
+
+
+
+
+        
+//
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+
+
+
+
+    }
+}
